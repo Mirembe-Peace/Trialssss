0908200676367847af2e6b1ea6ef19959cce82a5
Mirembe Peace Mercy <peace2de@gmail.com>
2024-08-26 12:02:08 +0300

    just learning

--- javas2.js
+++ javas2.js
@@ -8,3 +8,10 @@ let choiceOfColor = ['green','blue','red'];
 choiceOfColor[3] = 'purple'
 
 console.log(choiceOfColor[3]);
+console.warn('are you sure you want to delete');
+
+function greet(name){
+    console.log(`Hello ${name}`);
+}
+
+greet("John");
---
+++ javas3.js
@@ -0,0 +1,11 @@
+ //loops in javaScript
+ for (let i = 0; i<5; i++){
+    document.write("hello world <br>")
+ }
+ 
+ var x = 5
+ let y = 3
+ let a = x + y
+
+ document.write(a)
+
